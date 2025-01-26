7eb62cf88d6026a90e69d802af132681bfe81007
Linghe-Wang <linghe567@gmail.com>
2025-01-26 14:28:56 -0600

    Update figure and upload writing GIF from dataset

--- website/static/js/index.js
+++ website/static/js/index.js
@@ -11,10 +11,24 @@ $(document).ready(function() {
 			infinite: true,
 			autoplay: true,
 			autoplaySpeed: 9999999,
+			pagination: false,
     }
 
 		// Initialize all div with carousel class
-    var carousels = bulmaCarousel.attach('.carousel', options);
+    var carousels = bulmaCarousel.attach('#iterative_writing', options);
+
+	var option2 = {
+		slidesToScroll: 1,
+		slidesToShow: 1,
+		loop: true,
+		infinite: true,
+		autoplay: true,
+		autoplaySpeed: 9999999,
+	}
+
+	// Initialize all div with carousel class
+var carousels = bulmaCarousel.attach('#motivation', option2);
+
 	
     bulmaSlider.attach();
 
