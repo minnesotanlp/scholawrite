78d80a835c1d8fd74cd5279258caba4c40dc0d3c
Linghe-Wang <linghe567@gmail.com>
2025-01-25 23:36:30 -0600

    Format before text and after text in dataset example; Include iterative writing demos

--- website/static/js/index.js
+++ website/static/js/index.js
@@ -5,17 +5,18 @@ $(document).ready(function() {
     // Check for click events on the navbar burger icon
 
     var options = {
-			slidesToScroll: 1,
-			slidesToShow: 1,
+			slidesToScroll: 3,
+			slidesToShow: 3,
 			loop: true,
 			infinite: true,
 			autoplay: true,
-			autoplaySpeed: 5000,
+			autoplaySpeed: 9999999,
     }
 
 		// Initialize all div with carousel class
     var carousels = bulmaCarousel.attach('.carousel', options);
 	
     bulmaSlider.attach();
+
 
 })
